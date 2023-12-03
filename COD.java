ba6a7795f3d668b6f948628711bdd0440743e368
azmhhdal <azimahhuda@gmail.com>
2023-12-04 01:05:22 +0700

    Payment method

--- COD.java
+++ COD.java
@@ -1,3 +1,35 @@
-public interface COD {
-    
+/**
+ * @author Kelompok 4
+ * @version 1.0
+ * @since 2023-12-01
+ */
+public class COD extends Pembayaran {
+
+    /**
+     * Processes the payment using Cash On Delivery (COD).
+     * Displays a redirecting message, payment information, and a success message.
+     *
+     * @param username   The username on the marketplace.
+     * @param totalHarga The total amount to be paid.
+     */
+    @Override
+    public void prosesPembayaran(String username, int totalHarga) {
+        System.out.println("\n=> Requesting Cash On Delivery payment method . . .\n");
+
+        for(int i = 0; i <= 100000; i++){
+            if(i/20000 == 0){
+                continue;
+            }
+            System.out.print("\rRedirecting ... " + i/20000);
+        }
+        bersihkanConsole();
+
+        System.out.println("=".repeat(10) + " Cash On Delivery " + "=".repeat(10));
+        System.out.println("\nPayment Information\n");
+        System.out.println("Username on marketplace: " + username);
+        System.out.println("Total: " + totalHarga);
+        System.out.println("");
+        System.out.println("=".repeat(39));
+        System.out.println("\n=> Cash On delivery payment method requested successfully");
+    }
 }
--- Qris.java
+++ Qris.java
@@ -1,6 +1,52 @@
-public interface Qris {
+import java.io.IOException;
 
-    
-} 
-    
+/**
+ * @author Kelompok 4
+ * @version 1.0
+ * @since 2023-12-01
+ */
+public class Qris extends Pembayaran {
 
+    /**
+     * Implementation of the payment processing logic using QRIS.
+     *
+     * @param username   The username of the customer making the payment.
+     * @param totalHarga The total price to be paid.
+     */
+    @Override
+    public void prosesPembayaran(String username, int totalHarga) {
+        bersihkanConsole();
+
+        // Display QR code
+        System.out.println("Kindly scan this QR\n");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+        System.out.println("");
+
+        // Simulate redirection
+        for (int i = 0; i <= 10000; i++) {
+            if (i / 2000 == 0) {
+                continue;
+            }
+            System.out.print("\rRedirecting ... " + i / 2000);
+        }
+
+        // Input QRIS PIN
+        System.out.print("\nInput your QRIS PIN: ");
+        input.nextInt();
+
+        bersihkanConsole();
+
+        // Display payment information
+        System.out.println("=".repeat(10) + " QRIS " + "=".repeat(10));
+        System.out.println("\nPayment Information\n");
+        System.out.println("Username on marketplace: " + username);
+        System.out.println("Total: " + totalHarga);
+        System.out.println("");
+        System.out.println("=".repeat(26));
+    }
+}
